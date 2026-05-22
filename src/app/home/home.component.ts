730a8bdd71e4d4e386ba8025b71a2e152d9e4532
diana <diana.pimen.n@gmail.com>
2026-05-21 18:26:19 -0600

    sincronnizacion de los archivos de ambiente para produccion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MetaService } from '../services/meta-service.service';
 import { Meta } from '../models/meta.model';
 
